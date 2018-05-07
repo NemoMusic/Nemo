99833e271bec2c0ab97460d5d3c1b3f3400fb1d5
farukarakaya <farukarakaya@gmail.com>
2018-05-07 20:54:14 +0300

    sql

--- db_manager.py
+++ db_manager.py
@@ -7,10 +7,12 @@ connection = pymysql.connect(host='nemo.cnj8noexhne9.eu-west-1.rds.amazonaws.com
                             password='nemoadmin',
                             db='nemodb')
 
+
 def execute_sql(sql):
     cursor = connection.cursor()
     cursor.execute(sql)
     return cursor.fetchone()
+
 
 def test_connection():
     result = execute_sql("SELECT VERSION()")
@@ -18,10 +20,36 @@ def test_connection():
         return True
     else:
         return False
-#add here methods which calls sql
 
-##Connection Test
-#print(test_connection())
+
+create_user_table = """create table user(
+            id             int not null auto_increment,
+            email          varchar(50) not null unique,
+            name           varchar(20) not null,
+            last_name      varchar(20) not null,
+            gender         varchar(10),
+            user_name      varchar(20) not null unique,
+            password       varchar(20) not null,
+            wallet         numeric(8,2),
+            birth_date	   date,
+            primary key (id)) engine = INNODB;"""
+
+
+create_event_table="""create table event(
+            id          varchar(10) not null auto_increment,
+            name		varchar(50) not null,
+            date        date not null,
+            location    varchar(50) not null,
+            about       varchar(140),
+            primary key (id)) engine = INNODB;"""
+
+
+create_artist_table = """create table artist(
+		    user_id                     int  primary key,
+		    account_validation_date     date,
+		    primary key (user_id),
+		    foreign key (user_id) references user(id)) engine INNODB;"""
+
 
 
 
