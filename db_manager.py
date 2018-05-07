f1773b7694e7617dd31afb279052a84545ef57da
farukarakaya <farukarakaya@gmail.com>
2018-05-07 21:20:45 +0300

    table creation sqls

--- db_manager.py
+++ db_manager.py
@@ -48,7 +48,64 @@ create_artist_table = """create table artist(
 		    user_id                     int  primary key,
 		    account_validation_date     date,
 		    primary key (user_id),
-		    foreign key (user_id) references user(id)) engine INNODB;"""
+		    foreign key (user_id) references user(id)) engine = INNODB;"""
+
+
+create_playlist_table ="""create table playlist(
+            id              varchar(10)  not null auto_increment,
+            title           varchar(20) not null,
+            create_date     date(20),
+            is_private      varchar(10) not null,
+            user_id         varchar(20) not null,
+            primary key (id),
+            foreign key (user_id) references user(id)) engine = INNODB;"""
+
+
+create_album_table="""create table album(
+            id		        varchar(10)  not null auto_increment,
+            title			varchar(20) not null,
+            release_date	date(20),
+            price			int not null,
+            primary key (id)) engine = INNODB;"""
+
+
+create_song_table = """create table song(
+            id			    varchar(10)  not null auto_increment,
+            title			varchar(20) not null,
+            release_date	date(20) not null,
+            duration		time(5) not null,
+            number_of_listen	int,
+            price			int,
+            album_id		varchar(10) not null,
+            primary key (id),
+            foreign key (album_id) references album(id))engine = INNODB;"""
+
+
+create_genre_table = """create table genre(
+		    name     varchar(10) not null,
+		    primary key (name))engine = INNODB;"""
+
+
+create_activity_table = """create table activity(
+            id			    varchar(10) not null auto_increment,
+            date			date(10) not null,
+            entitiy_type	varchar(5) not null,
+            action_type		varchar(5) not null,
+            user_id 		varchar(10) not null,
+            primary key (id),
+            foreign key (user_id) references user(id))engine = INNODB;"""
+
+
+create_share_table = """create table share(
+            activity_id			varchar(10) not null,
+            share_comment		varchar(50),
+            foreign key (activity_id) references activity(id))engine = INNODB;"""
+
+
+create_follow_table = """create table follow(
+		activity_id int not null ,
+		primary key (activity_id),
+		foreign key (activity_id) references activity(id))engine = INNODB;"""
 
 
 
