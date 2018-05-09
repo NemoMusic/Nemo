0d2fa9af04c02d099b7b1caa0282ee43d39dc2af
merayman <musab.erayman@gmail.com>
2018-05-09 17:39:45 +0300

    db methods to fill, deadline: 10th may, 8 am.

---
+++ db_methods.py
@@ -0,0 +1,85 @@
+import pymysql
+import email
+
+#SQL queries module
+'''
+    Each function's inputs will be given by developers
+    Herkes kendi doldurmasi gereken methodu dolduracak
+'''
+# musab erayman
+def create_user():
+    return
+def remove_user():
+    return
+def create_artist():
+    return
+def remove_artist():
+    return
+def create_song():
+    return
+def remove_song():
+    return
+# ali bulut
+def create_playlist():
+    return
+def remove_playlist():
+    return
+def add_song_to_playlist():
+    return
+def remove_song_from_playlist():
+    return
+def create_event():
+    return
+def remove_event():
+    return
+def attend_to_event():
+    return
+# omer faruk karakaya
+def follow_user():
+    return
+def unfollow_user():
+    return
+def follow_playlist():
+    return
+def unfollow_playlist():
+    return
+# kerem ayoz
+def rate_song():
+    return
+def rate_playlist():
+    return
+def rate_album():
+    return
+def create_genre():
+    return
+def remove_genre():
+    return
+def create_album():
+    return
+def remove_album():
+    return
+# musab erayman
+def purchase_song():
+    return
+def purchase_album():
+    return
+def add_money_to_wallet():
+    return
+# ali bulut
+def share_song():
+    return
+def share_playlist():
+    return
+def share_album():
+    return
+# omer faruk karakaya
+def comment_on_song():
+    return
+def comment_on_playlist():
+    return
+def comment_on_album():
+    return
+def comment_on_event():
+    return
+def reply_to_comment():
+    return
