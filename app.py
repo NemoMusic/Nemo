969d2a7959b9369ecf774436d15140fc383c9c2b
farukarakaya <farukarakaya@gmail.com>
2018-05-09 18:44:36 +0300

    login-oprations

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import os as os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import pymysql
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('welcome.html')
+def hello():
+    if 'user_id' in session:
+        return redirect("/home")
+    else:
+        return redirect('/login')
+
+@app.route('/login')
+def loginPage():
+    return render_template('login.html')
 
 
 @app.route('/home', methods=["POST", "GET"])
 def loginSignin():
     username = request.form["loginEmail"]
     password = request.form["loginPassword"]
-    print(username)
     return render_template('welcome.html')
 
 
